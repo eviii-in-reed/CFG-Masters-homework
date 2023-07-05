43a3aebd5a7807a55c57ec803876fc25fb1f73a7
Wei_Lu <91103724+eviii-in-reed@users.noreply.github.com>
2023-07-05 21:16:32 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import MyButton from './src/components/myButton';
@@ -6,33 +7,41 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomePage from './src/screens/home';
 import LogIn from './src/screens/login';
-import React from 'react';
+import Categories from './src/screens/categories';
+import NewsDisplay from './src/screens/newsDisplay';
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import rootReducer from './src/reducers/rootReducer';
 
-// stall pages on top of each other
+
 const Stack = createStackNavigator();
+
+function reducer(state, action) {
+	if (action.type === "changeUsername") {
+		return { username: action.payload };
+	} else {
+		return state;
+	}
+}
+
+const store = createStore(rootReducer);
+
 
 export default function App({ navigation }) {
   const smiley = String.fromCodePoint(0x1F603);
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='home' component={HomePage}/>
-        <Stack.Screen name='login' component={LogIn}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-      // <Text style={styles.text}>My News App</Text>
-      //<Text style={styles.text1}>Hello this is an app full of fake news {smiley}</Text>
-      //<Text></Text>
-      //<Text></Text>
-      //<Text></Text>
-      //<MyButton title='button1' color = 'green'/>
-      //<MyButton title='button2' color = 'blue'/>
-      //<MyButton title='button3' color = 'purple'/>
-      //<CustomButton color="#E74C3C" title=" click me or regret " />
-      //<StatusBar style="auto"/> */}
-    // </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='home' component={HomePage}/>
+          <Stack.Screen name='login' component={LogIn}/>
+          <Stack.Screen name='categories' component={Categories}/>
+          <Stack.Screen name='display' component={NewsDisplay}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
